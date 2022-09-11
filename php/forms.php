c8408d0d48a29b561de9bc9c592ced61baab8c74
Stanislav <macmx@onemx.local>
2022-09-11 22:51:34 +0200

    Change Post to Get

--- php/forms.php
+++ php/forms.php
@@ -1,8 +1,8 @@
 <?php 
     $to .= "8800230@gmail.com";
     $subject .= "Data Test";
-    $message .= "Phone: ".$_POST['tel']."<br>";
-    $message .= "E-mail: ".$_REQUEST['email']."<br>";
+    $message .= "Phone: ".$_GET['tel']."<br>";
+    $message .= "E-mail: ".$_GET['email']."<br>";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
     mail($to, $subject, $message, $headers);
 ?>
